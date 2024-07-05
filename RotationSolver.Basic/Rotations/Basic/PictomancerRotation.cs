59fb57a1b49b229039e3ddde3e78947510de2253
Hagazzi Viliana <hagazzi.viliana@gmail.com>
2024-07-05 04:53:47 +0200

    Adding Base Picto

---
+++ RotationSolver.Basic/Rotations/Basic/PictomancerRotation.cs
@@ -0,0 +1,24 @@
+namespace RotationSolver.Basic.Rotations.Basic;
+
+partial class PictomancerRotation
+{
+    /// <inheritdoc/>
+    public override MedicineType MedicineType => MedicineType.Intelligence;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public override bool CanHealSingleSpell => false;
+
+
+    #region JobGauge
+	
+    #endregion
+
+    /// <inheritdoc/>
+    public override void DisplayStatus()
+    {
+
+    }
+
+}
